89e59794bd4b8fb32d3aefab3bb438eb41d75f46
yeodohyun <yeodo95@naver.com>
2019-02-15 02:12:54 +0900

    Ai FSM 구현 , 맵에서 특정 위치로 이동

---
+++ 스크립트/Character/AiBase.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiBase : ObjectBase
+{
+    NavMeshAgent m_Agent;
+
+    public float RangeSearching;
+    public GameObject Targets_Root;
+    public GameObject[] Target;
+
+    public enum State : int{ Idle, Chasing, Moving };
+
+    public State AiState;
+
+    public int TargetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+
+        AiState = State.Idle;
+
+        Target = new GameObject[Targets_Root.transform.childCount];
+        for (int i = 0; i < Targets_Root.transform.childCount; i++) {
+            Target[i] = Targets_Root.transform.GetChild(i).gameObject;
+        }
+
+        FindTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (AiState == State.Idle)
+        {
+            FindTarget();
+        }
+        //if (Target[TargetPos].transform.position == transform.position)
+        //{
+        //    AiState = State.Idle;
+        //}
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0.5f, 0.5f, 0.5f);
+        Gizmos.DrawSphere(transform.position, RangeSearching);
+    }
+
+    public void FindTarget()
+    {
+        AiState = State.Moving;
+        TargetPos = Random.Range(0, Target.Length);
+        m_Agent.destination = Target[TargetPos].transform.position;
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            if(other.gameObject == Target[TargetPos])
+            {
+                Debug.Log(other.name);
+                AiState = State.Idle;
+            }
+        }
+    }
+}
--- 스크립트/Character/CharacterBase.cs
+++ 스크립트/Character/CharacterBase.cs
@@ -7,7 +7,7 @@ public class CharacterBase : ObjectBase
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
