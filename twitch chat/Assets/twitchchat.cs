05204a2f5840ba1992db2ae734a4910e8a10b369
ParkYeeeeeon <py7161@naver.com>
2019-05-09 23:00:44 +0900

    트위치 채팅을 통한 투표반영  - 타이머를 넣어서 투표 제한시간 두기  - 특정 문자열을 잡아서 해당값 카운트  - 채팅창 캔버스 다차면 두 줄씩 지워주기  - 투표전의 랜덤값은 다른 딕셔너리로 관리하여 실행시킬 수 있게 하였음.

--- twitch chat/Assets/twitchchat.cs
+++ twitch chat/Assets/twitchchat.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 using static System.Random;
 
 
+
 public class twitchchat : MonoBehaviour
 {
     private TcpClient twitchClient;
@@ -20,47 +21,108 @@ public class twitchchat : MonoBehaviour
     public Text chatBox;
 
     //C#컨테이너 생성부
-    List<string> chatresultID = new List<string>();
-    List<string> rchatresultID = new List<string>();
+    //List<string> chatresultID = new List<string>();
+    Dictionary<int, string> chatresultID = new Dictionary<int, string>();
+    Dictionary<int, string> rchatresultID = new Dictionary<int, string>();
+    int[] parsChatData = new int[4] { 0, 0, 0, 0 };
+    List<string> parsChatNick = new List<string>();
+    Queue<string> chatData = new Queue<string>();
+    //실행시킬 결과값(결과값 모음은 rchatresultID안에 들어있음)
+    //excute 변수값으로 딕셔너리 rchatresultID를 대응시키고 value(string)을 가져와서 그 값을 실행시키면 됨.
+    int excute = 0;
+
+    public enum ChatStateType
+    {
+        none = 0,
+        chatRecvData,
+    }
+    public int chatStatus = 0;
+    //List<string> rchatresultID = new List<string>();
+
+    //타이머 GUI 구현
+    public Text timeText;
+    private float time;
 
 
 
     void Start()
     {
         Connect();
-                            
-        chatresultID.Add("1path");
-        chatresultID.Add("2path");
-        chatresultID.Add("3path");
-        chatresultID.Add("4path");
-        chatresultID.Add("5path");
-        chatresultID.Add("6path");
-        chatresultID.Add("7path");
+        time = 15f;
 
     }
 
-    
+    IEnumerator timecheck()
+    {
+        while (true)
+        {
+            if (time > 0)
+                time -= 60 * Time.deltaTime;
+
+            timeText.text = Mathf.Ceil(time).ToString();
+            yield return new WaitForSeconds(1);
+
+            if (time <= 0)
+            {
+                chatStatus = 0;
+                SendChat_content("투표가 종료되었습니다.");
+                timeText.text = " ";
+                time = 15f;
+                int max = 0;
+               
+                StopCoroutine(timecheck());
+                for (int i = 1; i < 4; ++i)
+                {
+                    string content = i + "이 " + parsChatData[i] + "회";
+                    SendChat_content(content);
+                    if(max< parsChatData[i])
+                    {
+                        max = parsChatData[i];
+                        excute = i;
+                    }
+                    
+                }
+                break;
+            }
+        }
+
+
+
+    }
+
     void Update()
     {
-     if(!twitchClient.Connected)
+        if (!twitchClient.Connected)
         {
             Connect();
         }
 
         ReadChat();
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && chatStatus == (int)ChatStateType.none)
         {
-           
 
+            chatStatus = (int)ChatStateType.chatRecvData;
             SendChat_content("투표를 시작합니다.");
             //고른 값 출력
             PatternShuffle();
-            for(int i=0;i<3;i++)
+            foreach (var data in rchatresultID)
             {
-                SendChat_vote( "#"+(i+1) + "  " + rchatresultID[i].ToString());
+                SendChat_vote("#" + (data.Key) + "  " + data.Value.ToString());
             }
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    SendChat_vote("#" + (i + 1) + "  " + rchatresultID[i].ToString());
+            //}
             SendChat_content("1~3번 중 하나를 골라 #(숫자)투표해주세요.");
+
+            StartCoroutine(timecheck());
+
+
+
         }
+
+
 
     }
 
@@ -77,13 +139,28 @@ public class twitchchat : MonoBehaviour
         writer.Flush();
     }
 
+    public void viewChat(string intputData)
+    {
+        chatData.Enqueue(intputData);
+        if (chatBox.cachedTextGenerator.lineCount >= 11 && chatData.Count != 0)
+        {
+            chatData.Dequeue();
+            chatData.Dequeue();
+        }
+        chatBox.text = "";
+        foreach (var data in chatData)
+        {
+            chatBox.text = chatBox.text + "\n" + data.ToString();
+        }
+    }
+
     public void SendChat_content(string data)
     {
         string message_to_send = "PRIVMSG #" + channelName + " :" + data;
         writer.WriteLine(message_to_send);
         writer.Flush();
         print(String.Format("{0}: {1}", "BotAI", data));
-        chatBox.text = chatBox.text + "\n" + String.Format("{0}: {1}", "BotAI", data);
+        viewChat(String.Format("{0}: {1}", "BotAI", data));
     }
 
     public void SendChat_vote(string data)
@@ -92,7 +169,7 @@ public class twitchchat : MonoBehaviour
         writer.WriteLine(message_to_send);
         writer.Flush();
         print(String.Format("{0}: {1}", "BotAI", data));
-        chatBox.text = chatBox.text + "\n" + String.Format("{0}: {1}", "BotAI", data);
+        viewChat(String.Format("{0}: {1}", "BotAI", data));
     }
 
     private void ReadChat()
@@ -112,38 +189,106 @@ public class twitchchat : MonoBehaviour
                 splitPoint = message.IndexOf(":", 1);
                 message = message.Substring(splitPoint + 1);
                 print(String.Format("{0}: {1}", chatName, message));
-                chatBox.text = chatBox.text + "\n" + String.Format("{0}: {1}", chatName, message);
-               
+                viewChat(String.Format("{0}: {1}", chatName, message));
+                //chatBox.text = chatBox.text + "\n" + String.Format("{0}: {1}", chatName, message);
+
+                // 투표 상태를 수집 할 경우
+                if (chatStatus == (int)ChatStateType.chatRecvData)
+                {
+
+                    for (int i = 1; i < 4; ++i)
+                    {
+                        if (message.Contains("#" + i) == true && parsChatNick.Contains(chatName) == false)
+                        {
+                            parsChatNick.Add(chatName);
+                            parsChatData[i]++;
+                        }
+                    }
+
+                }
+
             }
-       
         }
     }
     public void PatternShuffle()
     {
-        rchatresultID = ShuffleList<string>(chatresultID);
-        //Debug.Log(ShuffleList<int>(chatresultID));
-        for(int i=0;i<3;i++)
-        Debug.Log(rchatresultID[i]);
+        for (int i = 0; i < 4; ++i)
+        {
+            parsChatData[i] = 0;
+        }
+        parsChatNick.Clear();
+        chatresultID.Clear();
+        chatresultID.Add(1, "1path");
+        chatresultID.Add(2, "2path");
+        chatresultID.Add(3, "3path");
+        chatresultID.Add(4, "4path");
+        chatresultID.Add(5, "5path");
+        chatresultID.Add(6, "6path");
+        chatresultID.Add(7, "7path");
+        chatresultID.Add(8, "8path");
+
+        rchatresultID = ShuffleList(chatresultID);
+
+        foreach (var data in rchatresultID)
+        {
+            Debug.Log(data.Key + ", " + data.Value);
+        }
+
     }
 
-    private List<E>ShuffleList<E>(List<E>inputList)
+    private Dictionary<int, string> ShuffleList(Dictionary<int, string> inputList)
     {
-        List<E> randomList = new List<E>();
-
         System.Random r = new System.Random();
         int randomIndex = 0;
-        while(inputList.Count>2)
+        int maxCount = inputList.Count;
+        while (inputList.Count > 3)
         {
-            randomIndex = r.Next(0, inputList.Count);
-            randomList.Add(inputList[randomIndex]);
-            inputList.RemoveAt(randomIndex);
+            randomIndex = r.Next(0, maxCount);
+            inputList.Remove(randomIndex);
         }
-        
-        
-        return randomList;
-       
+
+        Dictionary<int, string> resultList = new Dictionary<int, string>();
+        int keyCount = 1;
+        // 랜덤 추출한 데이터를 새롭게 넣어준다 1~3번 을 출력 하기 위하여
+        foreach (var data in inputList)
+        {
+            resultList.Add(keyCount, data.Value);
+            ++keyCount;
+        }
+
+        return resultList;
     }
 
+ 
 
-   
+    //private List<E> ShuffleList<E>(List<E> inputList)
+    //{
+    //    List<E> randomList = new List<E>();
+
+    //    System.Random r = new System.Random();
+    //    int randomIndex = 0;
+    //    while (inputList.Count > 2)
+    //    {
+    //        randomIndex = r.Next(0, inputList.Count);
+    //        randomList.Add(inputList[randomIndex]);
+    //        inputList.RemoveAt(randomIndex);
+    //    }
+
+
+    //    return randomList;
+
+    //}
+
+    //public void timecheck()
+    //{
+    //    if (time > 0)
+    //        time -= Time.deltaTime;
+    //
+    //    timeText.text = Mathf.Ceil(time).ToString(); 
+    //
+    //
+    //}
+
+
+
 }
