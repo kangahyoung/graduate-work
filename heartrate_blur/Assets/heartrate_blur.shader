1b884f18bcaadf896ba586420b004eb76b93b2fc
ParkYeeeeeon <py7161@naver.com>
2019-08-08 13:08:01 +0900

    심박수 블러 효과 체크

---
+++ heartrate_blur/Assets/heartrate_blur.shader
@@ -0,0 +1,63 @@
+﻿Shader "ImageEffect/heartrate"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+	}
+		SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+		Fog{Mode off}
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 3.0
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+
+			sampler2D _MainTex;
+			half4 _MainTex_TexelSize; //텍셀이 차지하는 크기
+			half2 _BlurCenterPos; //블러의 중심위치
+			half _BlurSize; //블러를 위해 이미지가 이동된 거리
+			half _Samples; //샘플링 수
+
+			fixed4 frag(v2f_img IN) : SV_Target
+			{
+				half4 col = half4(0,0,0,1);
+				half2 movedTexcoord = IN.uv - _BlurCenterPos;
+
+				for (int i = 0; i < _Samples; i++)
+				{
+					half Scale = 1.0f - _BlurSize * _MainTex_TexelSize.x * i;
+					col.rgb += tex2D(_MainTex, movedTexcoord * Scale + _BlurCenterPos).rgb;
+				}
+				col.rgb *= 1 / _Samples;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ heartrate_blur/Assets/heartrate_resize.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+
+public class heartrate_resize: MonoBehaviour
+{
+    public Material heartrate;
+    public float blurSize = 0.08f;
+    public Vector2 blurCenterPos = new Vector2(0.5f, 0.5f);
+    [Range(1, 48)]
+    public int samples = 0;
+    public Material radialBlurMaterial = null;
+    public int time_measure;
+    public static float time_level = 0.08f;
+    public int middle = 21;
+    public int max = 48;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine("blursizecontrol");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (blurSize > 0.0f)
+        {
+            radialBlurMaterial.SetInt("_Samples", samples);
+            radialBlurMaterial.SetFloat("_BlurSize", blurSize);
+            radialBlurMaterial.SetVector("_BlurCenterPos", blurCenterPos);
+            Graphics.Blit(source, destination, radialBlurMaterial);
+
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+    IEnumerator blursizecontrol()
+    {
+        bool nowblur = false;
+        samples = 21;
+
+        time_measure = 0;
+        middle = 21;
+        max = 48;
+        StartCoroutine("timecontrol");
+
+        while (true)
+        {
+            if (nowblur == false && samples >= middle)
+            {
+                samples += 1;
+                blurSize += 0.1f;
+                if (samples >= max)
+                {
+                    nowblur = true;
+                }
+            }
+            else if (nowblur == true && samples <= max)
+            {
+                samples -= 1;
+                blurSize -= 0.1f;
+                if (samples <= middle)
+                {
+                    nowblur = false;
+                }
+            }
+
+
+
+            yield return new WaitForSeconds(time_level);
+        }
+
+    }
+
+
+    IEnumerator timecontrol()
+    {
+
+        while (true)
+        {
+
+            time_measure++;
+            if (time_measure == 30)
+            {
+
+                middle = middle - 7;
+                max = max - 16;
+                samples = (middle + max) / 2;
+                time_measure = 0;
+                time_level = time_level + 0.04f;
+                if (middle <= 0 && max <= 0)
+                {
+                    samples = 0;
+                    time_level = 0.08f;
+                    StopCoroutine("blursizecontrol");
+                    middle = 21;
+                    max = 48;
+                    StopCoroutine("timecontrol");
+                }
+            }
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+}
